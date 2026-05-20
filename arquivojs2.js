53603999e0242d872aa03b7efefbb1365ca1fd7f
RafaelBravin <noobygold@outlook.com>
2026-05-20 20:36:41 -0300

    Add files via upload

---
+++ arquivojs2.js
@@ -0,0 +1,9 @@
+document.writeln("<h2>Olá Mundo!</h2>");
+//window.alert("Olá Mundo!");
+document.getElementById("Title"),
+innerHTML = "Olá Mundo!";
+
+let nome = window.prompt
+("Digite o seu nome: ");
+
+document.getElementById("Tiltle")
