ed9effcb90f48e9ea1acb6dc28d6be998dd31091
RafaelBravin <noobygold@outlook.com>
2026-05-20 20:44:38 -0300

    Add files via upload

--- arquivojs2.js
+++ arquivojs2.js
@@ -1,9 +1,12 @@
-document.writeln("<h2>Olá Mundo!</h2>");
+document.writeln("<h2>Olá Mundo! </h2>");
 //window.alert("Olá Mundo!");
-document.getElementById("Title"),
-innerHTML = "Olá Mundo!";
+document.getElementById("title").innerHTML = "Olá Mundo!";
 
-let nome = window.prompt
-("Digite o seu nome: ");
+/*let  nome = window.prompt("Digite seu nome: ");
 
-document.getElementById("Tiltle")
+document.getElementById("title").innerHTML = "Olá " + nome + "! Bem-vindo ao JavaScript!";*/
+
+function Cumprimentar() {
+    let nome = document.getElementById("nameInput").value;
+    document.getElementById("post").innerHTML = `Olá ${nome}! Bem-vindo a aula de JavaScript!`;
+}
